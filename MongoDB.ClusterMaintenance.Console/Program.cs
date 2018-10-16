01024248122777a42e69cf63a09718ad8ccccf3d
ExM <explorermachine@gmail.com>
2018-10-16 23:29:19 +0300

    get database names

---
+++ MongoDB.ClusterMaintenance.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace MongoDB.ClusterMaintenance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Parser.Default.ParseArguments<Options>(args)
+				.WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts));
+		}
+
+		private static void RunOptionsAndReturnExitCode(Options opts)
+		{
+			var client = new MongoClient(opts.ConnectionString);
+
+			foreach (var name in client.ListDatabaseNames().ToList())
+			{
+				Console.WriteLine(name);
+			}
+		}
+	}
+
+	class Options
+	{
+		[Option('c', "connection", Required = true, HelpText = "connection string to MongoDB cluster")]
+		public string ConnectionString { get; set; }
+	}
+}
