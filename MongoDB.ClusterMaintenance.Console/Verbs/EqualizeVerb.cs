dfb4bc2acaf2f5ec861a9af7bc541b9543dbc880
ExM <explorermachine@gmail.com>
2019-06-07 19:23:21 +0300

    fix default value

--- MongoDB.ClusterMaintenance.Console/Verbs/EqualizeVerb.cs
+++ MongoDB.ClusterMaintenance.Console/Verbs/EqualizeVerb.cs
@@ -9,13 +9,12 @@ namespace MongoDB.ClusterMaintenance.Verbs
 	[Verb("equalize", HelpText = "alignment size shards by moving bound of zones")]
 	public class EqualizeVerb: BaseCommandFileVerb
 	{
-		[Option("moveLimit", Required = false, Default = "", HelpText = "limit of moving data (Gb)")]
+		[Option("moveLimit", Required = false, Default = null, HelpText = "limit of moving data (Gb)")]
 		public long? MoveLimit { get; set; }
 		
 		public override Task RunOperation(IKernel kernel, CancellationToken token)
 		{
-
-			long? scaledMoveLimit = 0;
+			long? scaledMoveLimit = null;
 			if (MoveLimit.HasValue)
 				scaledMoveLimit = MoveLimit.Value * ScaleSuffix.Giga.Factor();
 			
