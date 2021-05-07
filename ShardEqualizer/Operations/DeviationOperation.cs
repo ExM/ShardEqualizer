5bdff4cfbc7b8fd967f5ff607f3ad8b2b2ce7f82
Anatoliy Koperin <a.koperin@gmail.com>
2021-05-08 00:19:19 +0300

    wip collection by shards

--- ShardEqualizer/Operations/DeviationOperation.cs
+++ ShardEqualizer/Operations/DeviationOperation.cs
@@ -42,6 +42,62 @@ namespace ShardEqualizer.Operations
 
 			var sizeRenderer = new SizeRenderer("F2", _scaleSuffix);
 
+			var allShards = allCollStats.Values.SelectMany(x => x.Shards.Keys).Distinct().OrderBy(x => x.ToString(), StringComparer.Ordinal).ToList();
+
+			var unShSizes = allCollStats.Where(x => !x.Value.Sharded).GroupBy(x => x.Value.Primary!.Value)
+				.ToDictionary(x => x.Key, x => x.Sum(s => s.Value.Size));
+
+			var minOtherSize = allCollStats.Where(x => x.Value.Sharded).Sum(x => x.Value.Size) / allShards.Count * 5 / 100;
+
+			var orderedShardedColls =
+				allCollStats.Where(x => x.Value.Sharded).OrderByDescending(x => x.Value.Size).ToList();
+
+			var bigShardedColls = orderedShardedColls.Where(x => x.Value.Size >= minOtherSize);
+
+			var otherSizes = orderedShardedColls.Where(x => x.Value.Size < minOtherSize)
+				.SelectMany(x => x.Value.Shards).GroupBy(x => x.Key).ToDictionary(x => x.Key, x => x.Sum(s => s.Value.Size));
+
+			Console.WriteLine($"DATA unsharded {string.Join(" ", bigShardedColls.Select(x => x.Key))} other");
+
+			foreach (var shard in allShards)
+			{
+				var cells = new List<string>();
+
+				cells.Add(unShSizes.TryGetValue(shard, out var size) ? sizeRenderer.Render(size) : "0");
+
+				foreach (var pair in bigShardedColls)
+				{
+					cells.Add(sizeRenderer.Render(pair.Value.Shards[shard].Size));
+				}
+
+				cells.Add(sizeRenderer.Render(otherSizes[shard]));
+
+				Console.WriteLine($"{shard} {string.Join(" ", cells)}");
+			}
+
+
+			/*
+			var allShards = allCollStats.Values.SelectMany(x => x.Shards.Keys).Distinct().OrderBy(x => x.ToString(), StringComparer.Ordinal).ToList();
+
+			Console.WriteLine($";{string.Join(";", allShards)}");
+
+			foreach (var (ns, collStat) in allCollStats.Where(x => x.Value.Sharded))
+			{
+				var cells = allShards.Select(shard => collStat.Shards.TryGetValue(shard, out var statOnShard) ? sizeRenderer.Render(statOnShard.Size) : "0").ToList();
+
+				Console.WriteLine($"{ns};{string.Join(";", cells)}");
+			}
+
+			foreach (var g in allCollStats.Where(x => !x.Value.Sharded).GroupBy(x => x.Key.DatabaseNamespace))
+			{
+				var nonSharded = g.Select(x => x.Value).GroupBy(x => x.Primary!.Value)
+					.ToDictionary(x => x.Key, x => x.Sum(s => s.Size));
+				var cells = allShards.Select(shard => nonSharded.TryGetValue(shard, out var size) ? sizeRenderer.Render(size) : "0").ToList();
+
+				Console.WriteLine($"no sharded in {g.Key};{string.Join(";", cells)}");
+			}
+			*/
+				/*
 			var report = createReport(sizeRenderer);
 			foreach (var (ns, collStats) in allCollStats)
 			{
@@ -58,6 +114,7 @@ namespace ShardEqualizer.Operations
 				Console.WriteLine(report.Render(layout.Columns));
 				Console.WriteLine();
 			}
+			*/
 		}
 
 		private BaseReport createReport(SizeRenderer sizeRenderer)
