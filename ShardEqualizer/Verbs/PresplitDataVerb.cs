9fc4b764a5fa93aadfd3cd7e64f07018faa77955
Anatoliy Koperin <a.koperin@gmail.com>
2021-05-08 23:54:22 +0300

    rename option

--- ShardEqualizer/Verbs/PresplitDataVerb.cs
+++ ShardEqualizer/Verbs/PresplitDataVerb.cs
@@ -9,7 +9,7 @@ namespace ShardEqualizer.Verbs
 	[Verb("presplit", HelpText = "Split and distribute existing chunks among zones.")]
 	public class PresplitDataVerb : BaseCommandFileVerb
 	{
-		[Option("renew", Required = false, Default = false, HelpText = "Recreate unchanged zones order.")]
+		[Option("renew-current-zones", Required = false, Default = false, HelpText = "Recreate unchanged zones order.")]
 		public bool Renew { get; set; }
 
 		protected override async Task RunOperation(IKernel kernel, CancellationToken token)
