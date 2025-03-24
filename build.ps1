bcadccc02b6f94e55cf510b11417c079e82dcc78
Maksim Dik <m.dik@smartcat.ai>
2025-03-24 09:16:05 +0100

    Cleanup

--- build.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = "Stop"
-$mainFolder = Resolve-Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
-
-& dotnet tool restore
-& dotnet paket restore
-& dotnet build -c Release
